565685a91f2abf4ec82f6ea7b6c0087042370368
brothercutter <siqi@Siqis-MacBook-Air.local>
2014-05-08 13:41:31 -0700

    now the gut example has been tested

--- MRF/loadDataFruitfly.m
+++ MRF/loadDataFruitfly.m
@@ -1,7 +1,16 @@
-X = imread('../data/embryo.png');
-X = imresize(X,0.25);
+doEmbryo = 0;
+doGut = 1;
+if doEmbryo
+  X = imread('../data/embryo.png');
+  X = imresize(X,0.25);
+elseif doGut
+  X = imread('../data/gut.png');
+  X = imresize(X,0.5);
+end
+
 X = mean(X,3);
 X = (255 - X)/255;
+X = histeq(X);
 figure;imagesc(X);
 nNodes = length(X(:));
 nStates = 2;
