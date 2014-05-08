ba304faf4772b7113d6fffb85c4cc7405c543c4f
brothercutter <siqi@Siqis-MacBook-Air.local>
2014-05-08 15:35:28 -0700

    siqi added noise for real

--- MRF/loadDataFruitfly.m
+++ MRF/loadDataFruitfly.m
@@ -1,16 +1,21 @@
 doEmbryo = 0;
-doGut = 1;
+doGut = 0;
+doTouching = 1;
+
 if doEmbryo
   X = imread('../data/embryo.png');
   X = imresize(X,0.25);
 elseif doGut
   X = imread('../data/gut.png');
   X = imresize(X,0.5);
+  X = histeq(X);
+elseif doTouching
+  X = imread('../data/touchingEmb.jpg');
+  X = imresize(X,0.07);
 end
 
 X = mean(X,3);
 X = (255 - X)/255;
-X = histeq(X);
 figure;imagesc(X);
 nNodes = length(X(:));
 nStates = 2;
--- MRF/toyExamples.m
+++ MRF/toyExamples.m
@@ -1,6 +1,17 @@
 
 %generateData;
-loadDataFruitfly;
+%loadDataFruitfly;
+
+loadMRI = 1;
+if loadMRI
+  X = imread('../Chan-Vese/brain.jpg');
+  X = mean(X,3);
+  X = X/255 + 0.01*randn(size(X,1),size(X,2));
+  figure;imagesc(X);colorbar 
+  nNodes = length(X(:));
+  nStates = 2;
+  [nRows,nCols] = size(X);
+end
 
 
 %% Run MRF
@@ -8,15 +19,20 @@ loadDataFruitfly;
  probA = ones(nNodes,nStates)*(1/nStates);
  edgeStruct = createImgStructure(X,nStates);
 
- MuEst = [0.1,0.5];
+ MuEst = [0,1];
  SigmaEst = [0.1,0.3];
-   for i = 1:50
+   for i = 1:20
      [nodePot,edgePot] = producePotentials(X,edgeStruct,MuEst,SigmaEst,probA);
      [nodeBelMF,edgeBelMF,logZMF] = UGM_Infer_MeanField(nodePot,edgePot,edgeStruct);
      [MuEst,SigmaEst] = em_max(nodeBelMF,X(:));
      MuEst
      SigmaEst
- 
+     figure;
+     imagesc(reshape(nodeBelMF(:,2),nRows,nCols));
+     colormap gray
+     title('Mean Field Estimates of Marginals');
+     fprintf('(paused)\n');
+     pause
    end
    solveMRF
 
@@ -24,7 +40,7 @@ loadDataFruitfly;
 stop
 
 %% Run Chan Vese
-%I = X;
+I = X;
 %I = histeq(X);
 % Customerized Mask
 m = zeros(size(I,1),size(I,2));
