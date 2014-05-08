34abb8b0a015620db4000b2ac1098c1ead619ea4
brothercutter <siqi@Siqis-MacBook-Air.local>
2014-05-08 16:32:44 -0700

    push everything

--- MRF/toyExamples.m
+++ MRF/toyExamples.m
@@ -1,8 +1,10 @@
+clear all
+close all
 addpath(genpath('../'));
 %generateData;
-%loadDataFruitfly;
+loadDataFruitfly;
 
-loadMRI = 1;
+loadMRI = 0;
 if loadMRI
   X = imread('../Chan-Vese/brain.jpg');
   X = mean(X,3);
@@ -13,27 +15,35 @@ if loadMRI
   [nRows,nCols] = size(X);
 end
 
+ MuEst = [0,1];
+ SigmaEst = [0.1,0.3];
+ i = 0;
+ diff = 1;
+ iter_max = 20;
 
 %% Run MRF
 
  probA = ones(nNodes,nStates)*(1/nStates);
  edgeStruct = createImgStructure(X,nStates);
 
- MuEst = [0,1];
- SigmaEst = [0.1,0.3];
-   for i = 1:20
+
+
+ while i<iter_max && diff>1e-3
+     MuEst_o = MuEst;
+     SigmaEst_o = SigmaEst;
+
      [nodePot,edgePot] = producePotentials(X,edgeStruct,MuEst,SigmaEst,probA);
      [nodeBelMF,edgeBelMF,logZMF] = UGM_Infer_MeanField(nodePot,edgePot,edgeStruct);
      [MuEst,SigmaEst] = em_max(nodeBelMF,X(:));
-     MuEst
-
-     SigmaEst
      figure;
      imagesc(reshape(nodeBelMF(:,2),nRows,nCols));
      colormap gray
      title('Mean Field Estimates of Marginals');
      fprintf('(paused)\n');
-
+     
+     MuEst
+     SigmaEst
+     diff = max(norm(MuEst_o-MuEst),norm(SigmaEst_o - SigmaEst))
    end
    solveMRF
 
@@ -43,11 +53,21 @@ end
 %% Run Chan Vese
 
 I = X;
-I = histeq(X);
 % Customerized Mask
 m = zeros(size(I,1),size(I,2));
 m(10:size(I,1)-10,10:size(I,2)-10) = 1;
-seg = chanvese(I,m,600,0.1); 
+seg = chenvese(I,m,600,0.1); 
+
+
+Y0 = X(find(seg == 0));
+Y1 = X(find(seg == 1));
+
+MuEst(1) = mean(Y0);
+MuEst(2) = mean(Y1);
+
+SigmaEst(1) = std(Y0);
+SigmaEst(2) = std(Y1);
+
 
 % Built-in Mask
 seg = chenvese(I,'large',600,0.1); 
